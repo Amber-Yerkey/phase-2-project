f90000b906605c0a4ef1ddc3be08d68501fc854c
Amber Frantz <amber.k.frantz@mgail.com>
2022-06-27 15:33:22 -0400

    search added

--- src/components/VillagerCard.js
+++ src/components/VillagerCard.js
@@ -1,15 +1,33 @@
 import React, { useContext } from 'react';
-import { Card, Grid, Image, ResponsiveContext } from 'grommet'
+import {  Box, Card, CardHeader, CardBody, Grid, Image, ResponsiveContext } from 'grommet'
+import { useNavigate } from "react-router-dom";
 
-function VillagerCard() {
+function VillagerCard({ name, image, id }) {
     const size = useContext(ResponsiveContext);
+    const navigate = useNavigate();
+
+    // function handleCardClick(e){
+    //      e.preventDefault();
+    //     navigate('/' + name);
+    // }
+
     return ( 
         <>
-            <Grid columns={size !== 'small' ? 'small' : '100%'} gap="small">
-                <Card align="center">Villager Name<Image src='https://acnhapi.com/v1/images/villagers/1' /></Card>
+        <Box fill pad="small" >
+            <Grid columns={size !== 'small' ? 'small' : '100%'} >
+                    <Card pad="medium" key={id} background='#f1f3c4' 
+                    hoverIndicator={{background: {color:'background-contrast'}
+                    ,elevation:'medium'}}>
+                    {/* onClick={handleCardClick}> */}
+                    <CardHeader><h4>{name}</h4></CardHeader>
+                    <CardBody><Image src={image} /></CardBody>
+                    </Card>
             </Grid>
+        </Box>
         </>
      );
 }
+
+// columns={size !== 'small' ? 'small' : '100%'}
 
 export default VillagerCard;
--- src/components/VillagerList.js
+++ src/components/VillagerList.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import VillagerCard from "./VillagerCard"
+import VillagerFilter from './VillagerFilter';
+
+
 
 function VillagerList() {
+
+
+const [villagerList, setVillagers] = useState([]);
+const [search, setSearch] = useState("");
+
+useEffect(() => {
+  fetch("https://acnhapi.com/v1a/villagers/")
+    .then((resp) => resp.json())
+    .then((data) => { setVillagers(data.map((villager) => {return villager})) })
+    .catch((error) => {console.error(error)})
+}, [])
+
+const villagerSearch = villagerList
+.filter((VillagerCard) => VillagerCard.name['name-USen'].toLowerCase().includes(search.toLowerCase()));
+
     return ( 
         <>
-        <div>Villagers</div>
-        {/* adding in a .map and passing to cards to populate them */}
-        <VillagerCard />
+        <h1 align="center">Villagers</h1>
+        <VillagerFilter search={search} setSearch={setSearch} />
+        {villagerSearch.map((villager) => 
+        <VillagerCard key={villager.id} name={villager.name['name-USen']} image={villager.icon_uri} />)}
+        
         </>
      );
 }
